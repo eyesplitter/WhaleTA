7e2c13434fd4a1df8ac4197aade70934179f44c5
Ivan S <eye.splitter@gmail.com>
2025-03-28 15:14:23 +0200

    add provider_id to historic prices

---
+++ backend/migrations/1743167124520_update-historic-prices.js
@@ -0,0 +1,24 @@
+/**
+ * @type {import('node-pg-migrate').ColumnDefinitions | undefined}
+ */
+exports.shorthands = undefined;
+
+/**
+ * @param pgm {import('node-pg-migrate').MigrationBuilder}
+ * @param run {() => void | undefined}
+ * @returns {Promise<void> | void}
+ */
+exports.up = (pgm) => {
+    pgm.addColumn('historical_crypto_prices', {
+        provider_id: { type: 'integer', notNull: true, references: 'providers' },
+    });
+};
+
+/**
+ * @param pgm {import('node-pg-migrate').MigrationBuilder}
+ * @param run {() => void | undefined}
+ * @returns {Promise<void> | void}
+ */
+    exports.down = (pgm) => {
+    pgm.dropColumn('historical_crypto_prices', 'provider_id');
+};
