5bc1cc736fe4b70a57071e3cd5dff48f7abc3433
Ivan S <eye.splitter@gmail.com>
2025-03-28 14:37:23 +0200

    simple get price api implementation

--- backend/migrations/1743160325233_update-providers.js
+++ backend/migrations/1743160325233_update-providers.js
@@ -9,11 +9,8 @@ exports.shorthands = undefined;
  * @returns {Promise<void> | void}
  */
 exports.up = (pgm) => {
-    pgm.renameColumn('providers', 'base_url', 'url');
-    pgm.addColumn('providers', {pair: { type: 'TEXT', notNull: true }});
-    pgm.addColumn('providers', {action: { type: 'TEXT', notNull: true }});
     pgm.sql (`
-        INSERT INTO providers (name, url, pair, action) VALUES ('coingecko', 'https://api.coingecko.com/api/v3/simple/price?ids=the-open-network&vs_currencies=usd', 'the-open-network/usd', 'price');
+        INSERT INTO providers (name, base_url) VALUES ('coingecko', 'https://api.coingecko.com/api/v3/simple/{action}?ids={currency}&vs_currencies=usd')
     `);
 };
 
@@ -23,9 +20,6 @@ exports.up = (pgm) => {
  * @returns {Promise<void> | void}
  */
 exports.down = (pgm) => {
-    pgm.renameColumn('providers', 'url', 'base_url');
-    pgm.dropColumn('providers', 'pair');
-    pgm.dropColumn('providers', 'action');
     pgm.sql (`
         DELETE FROM providers WHERE name = 'coingecko';
     `);
--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,5 +1,6 @@
-import express from "express"
+import express, { response } from "express"
 import dotenv from "dotenv"
+import { Pool } from "pg"
 dotenv.config()
 
 const config: Record<string, string | number> = {
@@ -14,9 +15,95 @@ const config: Record<string, string | number> = {
   redisPassword: process.env.REDIS_PASSWORD || "cryptoexchangepass",
 }
 
-const app = express()
+interface ProviderResponse {
+  [key: string]: {usd: number}
+}
 
+interface Price {
+  price: {[key: string]: {usd: number} }
+  providerId: number
+}
+  
+const app = express()
 app.use(express.json())
+
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+})
+
+const PAIRS_TO_CURRENCY_IDS: Record<string, string[]> = {
+  "ton-usdt": ["the-open-network", "tether"]
+}
+
+async function fetchPrice(currencies: string[]) {
+  try {
+    const providers = await getProviders()
+    for (const provider of providers) {
+      const url = provider.base_url.replace('{action}', 'price').replace('{currency}', currencies.join(','))
+      const response = await fetch(url)
+
+      if (!response.ok) {
+        console.warn(`Provider ${provider.name} failed with status: ${response.status} ${response.statusText}`)
+        continue
+      }
+      const data = await response.json() as ProviderResponse
+      return {
+        price: data,
+        providerId: provider.id
+      }
+    }
+
+  } catch (error) {
+    console.error("error fetchPrice", error.message)
+    throw new Error("error fetchPrice")
+  }
+
+  throw new Error("All providers failed")
+}
+
+async function savePrice(pair: string, price: number, providerId: number) {
+  try {
+    await pool.query(
+      "INSERT INTO crypto_prices (pair, price, timestamp, provider_id) VALUES ($1, $2, NOW(), $3) ON CONFLICT (pair) DO UPDATE SET price = EXCLUDED.price, timestamp = NOW()",
+      [pair, price, providerId],
+    )
+  } catch (error) {
+    console.error("error saveTonPrice", error.message)
+  }
+}
+
+async function getProviders() {
+  try {
+    const result = await pool.query("SELECT * FROM providers")
+    return result.rows
+  } catch (error) {
+    console.error("error getProviders", error.message)
+    throw new Error("error getProviders")
+  }
+}
+
+app.get("/price/:pair", async (req, res) => {
+  const pair = req.params.pair
+  try {
+    const currencies = PAIRS_TO_CURRENCY_IDS[pair]
+    
+    if(!currencies) {
+      throw new Error(`Pair ${pair} not found`)
+    }
+
+    const price = await fetchPrice(currencies)
+
+    const fromPrice = price.price[currencies[0]].usd
+    const toPrice = price.price[currencies[1]].usd
+    const pairPrice = fromPrice / toPrice;
+
+    await savePrice(pair, pairPrice, price.providerId);
+    res.json({ pair, price: pairPrice, reversePrice: 1 / pairPrice })
+  } catch (error) {
+    res.status(500).json({ error: error.message })
+  }
+})
+
 
 app.get("/api/health", (req, res) => {
   res.json({ status: "ok", message: "it works" })
